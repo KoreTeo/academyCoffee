77950ba7104b651d6fa4ecbe13918cc755c29878
ExToozy <ilya.makarov.04@mail.ru>
2023-05-22 15:14:11 +0700

    Настройка статических файлов django

--- academyCoffee/models.py
+++ academyCoffee/models.py
@@ -11,3 +11,16 @@ class Task(models.Model):
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
+
+
+
+class Task1(models.Model):
+    title = models.CharField('Назание', max_length=50)
+    task = models.TextField('Описание', max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
