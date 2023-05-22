eaf25cbcf31b25453e692414e5bd4396228e0c88
ExToozy <ilya.makarov.04@mail.ru>
2023-05-22 14:44:50 +0700

    Настройка django

--- academyCoffee/views.py
+++ academyCoffee/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'r.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+    form = TaskForm()
+
+    context = {
+        'form': form,
+        'error': error,
+    }
+    return render(request, 'main/create.html', context)
